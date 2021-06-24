e1954fcba2e7ec370f8ebe83ceb5df751be7c9c8
TEDU <83123095@qq.com>
2021-06-24 17:38:45 +0800

    MyBatisPlus依赖完成

---
+++ knows-portal/src/main/java/cn/tedu/knows/portal/model/Tag.java
@@ -0,0 +1,27 @@
+package cn.tedu.knows.portal.model;
+
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+
+@Data
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Slf4j
+public class Tag {
+
+    private Integer id;
+    private String name;
+    private String createby;
+    private String createtime;
+
+    public void record(){
+
+        log.info("这是Lombok@Slf4j注解的log对象输出的日志信息");
+
+    }
+
+}
--- knows-portal/src/test/java/cn/tedu/knows/portal/KnowsPortalApplicationTests.java
+++ knows-portal/src/test/java/cn/tedu/knows/portal/KnowsPortalApplicationTests.java
@@ -1,5 +1,6 @@
 package cn.tedu.knows.portal;
 
+import cn.tedu.knows.portal.model.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,13 @@ class KnowsPortalApplicationTests {
 
     @Test
     void contextLoads() {
+        Tag tag=new Tag();
+        tag.setId(1);
+        tag.setName("java基础");
+        System.out.println("id:"+tag.getId());
+        System.out.println("name:"+tag.getName());
+        System.out.println(tag);
+        tag.record();
     }
 
 }
