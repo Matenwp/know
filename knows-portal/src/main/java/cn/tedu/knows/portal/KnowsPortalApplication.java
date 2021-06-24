35f16d46c219cd3d72e9d0136f37ca1255acfb9e
TEDU <83123095@qq.com>
2021-06-24 15:03:25 +0800

    创建完毕

---
+++ knows-portal/src/main/java/cn/tedu/knows/portal/KnowsPortalApplication.java
@@ -0,0 +1,13 @@
+package cn.tedu.knows.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowsPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowsPortalApplication.class, args);
+    }
+
+}
