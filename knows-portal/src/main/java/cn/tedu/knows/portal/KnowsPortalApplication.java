5bf3d9bd1c19a5045202e1b0d39de821c695b4f3
TEDU <83123095@qq.com>
2021-06-25 09:53:10 +0800

    MyBatisPlus依赖完成2

--- knows-portal/src/main/java/cn/tedu/knows/portal/KnowsPortalApplication.java
+++ knows-portal/src/main/java/cn/tedu/knows/portal/KnowsPortalApplication.java
@@ -1,9 +1,14 @@
 package cn.tedu.knows.portal;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@MapperScan是MyBatis的注解
+//功能是指定一个包中所有的接口是MyBatis框架的mapper接口
+// 这样这个包中所有接口就不需要每个都使用@Mapper标记了
+@MapperScan("cn.tedu.knows.portal.mapper")
 public class KnowsPortalApplication {
 
     public static void main(String[] args) {
