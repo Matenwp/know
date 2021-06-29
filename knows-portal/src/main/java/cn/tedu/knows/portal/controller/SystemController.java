836c7187d2c59129bb62d2f0edf119b09d53ed5a
TEDU <83123095@qq.com>
2021-06-29 15:55:39 +0800

    SpringValidation验证

--- knows-portal/src/main/java/cn/tedu/knows/portal/controller/SystemController.java
+++ knows-portal/src/main/java/cn/tedu/knows/portal/controller/SystemController.java
@@ -6,6 +6,8 @@ import cn.tedu.knows.portal.service.IUserService;
 import cn.tedu.knows.portal.vo.RegisterVo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,8 +22,19 @@ public class SystemController {
     private IUserService userService;
 
     @PostMapping("/register")
-    public String registerStudent(RegisterVo registerVo){
-
+    public String registerStudent(
+            //@Validated表示这个方法运行前
+            //Spring Validation框架会对registerVo中的属性进行验证
+            // 验证规则就是这个类中编写的规则
+            @Validated RegisterVo registerVo,
+            //BindingResult就是接收验证结果和错误信息的类型
+            BindingResult result){
+        //如果SpringValidation验证有错误
+        if(result.hasErrors()){
+            //获得错误信息
+            String msg=result.getFieldError().getDefaultMessage();
+            return msg;
+        }
         log.debug("收到学生注册信息:{}",registerVo);
         try {
             userService.registerStudent(registerVo);
--- knows-portal/src/main/java/cn/tedu/knows/portal/vo/RegisterVo.java
+++ knows-portal/src/main/java/cn/tedu/knows/portal/vo/RegisterVo.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.experimental.Accessors;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import java.io.Serializable;
 
 @Data
@@ -14,12 +15,21 @@ public class RegisterVo implements Serializable {
     //注解汇总的message表示属性为空时的错误提示信息
     @NotBlank(message = "邀请码不能为空")
     private String inviteCode;
+
     @NotBlank(message = "手机号(用户名)不能为空")
+    //@Pattern表示下面属性值要符合指定正则表达式
+    //regexp执行正则表达式,message当属性值不符合正则时的错误信息
+    @Pattern(regexp = "^1\\d{10}$",message = "手机号格式不正确")
     private String phone;
+
     @NotBlank(message = "昵称不能为空")
+    @Pattern(regexp = "^.{2,20}$",message = "昵称是2~20个字符")
     private String nickname;
+
     @NotBlank(message = "密码不能为空")
+    @Pattern(regexp = "^\\w{6,20}$",message = "密码是6~20位数字字母'_'")
     private String password;
+
     @NotBlank(message = "密码确认不能为空")
     private String confirm;
 
