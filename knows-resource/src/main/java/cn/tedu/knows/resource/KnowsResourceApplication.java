e2dc65bee5abc32724c0b582f230b2ca8fcd2356
TEDU <83123095@qq.com>
2021-07-02 17:41:50 +0800

    创建静态资源服务器

---
+++ knows-resource/src/main/java/cn/tedu/knows/resource/KnowsResourceApplication.java
@@ -0,0 +1,13 @@
+package cn.tedu.knows.resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowsResourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowsResourceApplication.class, args);
+    }
+
+}
