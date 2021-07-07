df0dbd6b7dd33c52df34d06bc95d4265ae087abc
TEDU <83123095@qq.com>
2021-07-07 09:58:02 +0800

    MyBatis管理查询未完成

--- knows-portal/src/main/java/cn/tedu/knows/portal/mapper/AnswerMapper.java
+++ knows-portal/src/main/java/cn/tedu/knows/portal/mapper/AnswerMapper.java
@@ -4,15 +4,21 @@ import cn.tedu.knows.portal.model.Answer;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.springframework.stereotype.Repository;
 
-/**
-* <p>
-    *  Mapper 接口
-    * </p>
-*
-* @author tedu.cn
-* @since 2021-06-25
-*/
-    @Repository
-    public interface AnswerMapper extends BaseMapper<Answer> {
+import java.util.List;
 
-    }
+/**
+ * <p>
+ * Mapper 接口
+ * </p>
+ *
+ * @author tedu.cn
+ * @since 2021-06-25
+ */
+@Repository
+public interface AnswerMapper extends BaseMapper<Answer> {
+
+    // 按问题id查询包含所有评论的回答list
+    // 和xml文件对应的方法不用写任何注解
+    List<Answer> findAnswersWithCommentsByQuestionId(Integer id);
+
+}
