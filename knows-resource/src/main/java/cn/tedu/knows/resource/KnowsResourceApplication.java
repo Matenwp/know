12c3cbd42928188576e1c7ce5f5cf9061d96aaf2
TEDU <83123095@qq.com>
2021-07-08 17:32:49 +0800

    微服务父项目pom完成

--- knows-resource/src/main/java/cn/tedu/knows/resource/KnowsResourceApplication.java
+++ knows-resource/src/main/java/cn/tedu/knows/resource/KnowsResourceApplication.java
@@ -2,8 +2,10 @@ package cn.tedu.knows.resource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class KnowsResourceApplication {
 
     public static void main(String[] args) {
