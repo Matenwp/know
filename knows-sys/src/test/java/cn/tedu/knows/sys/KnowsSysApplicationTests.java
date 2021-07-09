d1ed197755e4a4fd00a6a2d2246fe8293d11c32f
TEDU <83123095@qq.com>
2021-07-09 16:13:53 +0800

    配置中心

--- knows-sys/src/test/java/cn/tedu/knows/sys/KnowsSysApplicationTests.java
+++ knows-sys/src/test/java/cn/tedu/knows/sys/KnowsSysApplicationTests.java
@@ -1,13 +1,20 @@
 package cn.tedu.knows.sys;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class KnowsSysApplicationTests {
 
+    @Value("${my.name}")
+    String name;
+    @Value("${class.name}")
+    String className;
     @Test
     void contextLoads() {
+        System.out.println(name);
+        System.out.println(className);
     }
 
 }
