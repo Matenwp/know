d64bd25f9a9eb3387e27a47e0613516de89a32f9
TEDU <83123095@qq.com>
2021-07-16 16:11:56 +0800

    SpringData分页查询

--- knows-search/src/main/java/cn/tedu/knows/search/repository/ItemRepository.java
+++ knows-search/src/main/java/cn/tedu/knows/search/repository/ItemRepository.java
@@ -1,11 +1,34 @@
 package cn.tedu.knows.search.repository;
 
 import cn.tedu.knows.search.vo.Item;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import java.util.List;
 
 public interface ItemRepository extends
         ElasticsearchRepository<Item,Long> {
     //    ElasticsearchRepository<Item,Long>中
     //Item是vo泛型类型
     //Long是vo对象的id类型
+
+    //根据title查询匹配的Item
+    Iterable<Item> queryItemsByTitleMatches(String title);
+
+    //查询多个结果返回值为Iterable 必须根据要求的语法定义方法名
+    //参数根据需要的条件项传入
+
+    //根据title和品牌进行查询
+    List<Item> queryItemsByTitleMatchesAndBrandMatches(
+            String title,String brand);
+    //根据title查询根据价格升序排序
+    List<Item> queryItemsByTitleMatchesOrderByPriceAsc(
+            String title);
+
+    //分页查询
+    Page<Item> queryItemsByTitleMatchesOrderByPriceAsc(
+            String title, Pageable pageable);
+
+
 }
--- knows-search/src/test/java/cn/tedu/knows/search/KnowsSearchApplicationTests.java
+++ knows-search/src/test/java/cn/tedu/knows/search/KnowsSearchApplicationTests.java
@@ -4,6 +4,8 @@ import cn.tedu.knows.search.repository.ItemRepository;
 import cn.tedu.knows.search.vo.Item;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
@@ -63,11 +65,65 @@ class KnowsSearchApplicationTests {
         //删除
         //itemRepository.deleteById();
         //修改直接使用Save方法用相同id的覆盖掉
+    }
 
+    //查询1
+    @Test
+    void  find1(){
+        Iterable<Item> items=itemRepository
+                .queryItemsByTitleMatches("游戏鼠标");
+        for(Item item: items){
+            System.out.println(item);
+        }
+    }
+
+    //查询2
+    @Test
+    void find2(){
+        Iterable<Item> items=itemRepository
+                .queryItemsByTitleMatchesAndBrandMatches(
+                        "鼠标","罗技");
+        for(Item item : items){
+            System.out.println(item);
+        }
+    }
+
+    //查询3:排序
+    @Test
+    void find3(){
+        Iterable<Item> items=itemRepository
+                .queryItemsByTitleMatchesOrderByPriceAsc("游戏");
+        items.forEach(item -> System.out.println(item));
 
     }
 
+    //分页
+    @Test
+    void page(){
+        //Page是包含查询结果和分页信息的对象
+        //PageRequest.of是一个方法,
+        // 参数是分页查询的页码和每页条数,注意页码从0开始
+        //Pageable是PageRequest.of方法的返回值类型
+        Page<Item> page=itemRepository
+                .queryItemsByTitleMatchesOrderByPriceAsc(
+                        "游戏", PageRequest.of(0,2));
+        for(Item item:page){
+            System.out.println(item);
+        }
+        //page.forEach(item -> System.out.println(item));
+        //输出分页信息
+        System.out.println("总页数:"+page.getTotalPages());
+        System.out.println("当前页:"+page.getNumber());
+        System.out.println("每页条数:"+page.getSize());
+        System.out.println("是不是首页:"+page.isFirst());
+        System.out.println("是不是末页:"+page.isLast());
+        System.out.println("下一页:"
+                +page.nextOrLastPageable().getPageNumber());
+        System.out.println("上一页:"
+                +page.previousOrFirstPageable().getPageNumber());
 
+
+    }
 
 
 }
